80b644c279a4c53de5278c76239e61e0afb7ec33
rupertbates <rupert.bates@guardian.co.uk>
2018-04-12 16:16:42 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+## A script to update all the Customers in a Stripe account
+
+To use create a `.env` file in the same directory as the script with your Stripe key in it in the format
+`STRIPE_KEY=sk_test_my_stripe_key`  
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').load();
 const fetch = require('node-fetch');
 const url = 'https://api.stripe.com/v1/customers';
-const Authorization = `Bearer ${process.env.STRIPE_KEY}`;
+const Authorization = `Bearer ${process.env.STRIPE_KEY}`; //Set this value in .env file in the same directory as this file
 let iterations = 0;
 let numOfCustomers = 0;
+let delay = 0; //Used to stagger requests
 
 function cleanAll(previousId) {
     return cleanPage(previousId)
@@ -18,13 +19,14 @@ function cleanAll(previousId) {
 
 function cleanPage(previousId) {
     iterations++;
+    console.log(`Fetching page ${iterations}`)
     const fetchUrl = `${url}?limit=100` + (previousId == undefined ? '' : `&starting_after=${previousId}`);
     return fetchCustomers(fetchUrl)
         .then(response => response.json())
         .then(json => {
             json.data
                 .filter(c => c.description != null && c.description != 'null')
-                .forEach(c => setTimeout(() => cleanCustomer(c.id), getDelay() * 1000));
+                .forEach(c => setTimeout(() => cleanCustomer(c.id), delay++ * 1000));
             if (json.has_more == true) {
                 return json.data[json.data.length - 1].id;
             } else {
@@ -42,11 +44,6 @@ function fetchCustomers(url) {
     });
 }
 
-let delay = 0;
-function getDelay(index, iteration) {
-    return delay++;
-}
-
 function cleanCustomer(customerId) {
     numOfCustomers++;
     console.log(`Cleaning ${customerId}`);
@@ -59,7 +56,7 @@ function cleanCustomer(customerId) {
         })
         .then(json => {
             if (json != undefined) {
-                console.log(`Successfully cleaned ${customerId}, new descriptionis ${json.description}`);
+                console.log(`Successfully cleaned ${customerId}, new description is ${json.description}`);
             } else {
                 console.error(`Failed to clean ${customerId}`);
             }
@@ -83,9 +80,8 @@ function logError(response) {
     });
 }
 
-//cleanCustomer('cus_CfQPiFclHdwoB3');
-//cleanAll();
-//stripeList();
-console.log(Authorization);
+cleanAll();
+
+
 
 
